f5d6876db192c4297c77d5de1a67a11f0aea2f2a
Michal Vician <vicianm@gmail.com>
2017-07-26 14:49:31 +0200

    DynamicHeaderLayout introduced

---
+++ src/main/java/sk/plaut/dynamicformheader/DynamicHeaderLayout.java
@@ -0,0 +1,26 @@
+package sk.plaut.dynamicformheader;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class DynamicHeaderLayout extends LinearLayout {
+
+    public DynamicHeaderLayout(Context context) {
+        super(context);
+    }
+
+    public DynamicHeaderLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DynamicHeaderLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public DynamicHeaderLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+}
